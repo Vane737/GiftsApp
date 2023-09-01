4cec3532b23b6f5a55d985ce6d0f457358909087
Vane737 <77525733+Vane737@users.noreply.github.com>
2023-08-31 22:25:37 -0600

    uso de HttpClient

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpParams } from '@angular/common/http';
 // const GIPHY_API_KEY = 'H7k1pmB5vmwKCg8JHR6C1w2avkDUTdam';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
   private _tagsHistory: string[] = [];
-  private apiKey: string = 'H7k1pmB5vmwKCg8JHR6C1w2avkDUTdam';
-  
-  constructor() { }
+  private apiKey:       string = 'H7k1pmB5vmwKCg8JHR6C1w2avkDUTdam';
+  private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
+
+  constructor( private http: HttpClient ) { }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -28,7 +29,18 @@ export class GifsService {
   searchTag( tag: string ):void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
-    console.log(this._tagsHistory);
+    // console.log(this._tagsHistory);
+    const params = new HttpParams()
+      .set( 'api_key', this.apiKey )
+      .set('limit', '10')
+      .set('q', tag)
+
+    this.http.get(`${this.serviceUrl}/search`, { params })
+    .subscribe( resp => {
+
+      console.log(resp);
+
+    })
 
   }
 
